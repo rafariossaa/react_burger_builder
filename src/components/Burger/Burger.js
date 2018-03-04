7a038893b8f4954bc6ddee8c1422538e7ea66b49
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2018-03-04 20:12:34 +0100

    (vid 112) Se crea la hamburgesa de forma dinamica, en Burger se implementa una transformación de objeto a array. El bug del marco persiste, creo que es de una linea en blanco que se mete en algun <div> (o algo asi).

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    //Transforma el objeto en un array
+    const tansformedIngedients = Object.keys(props.ingredients)
+            .map(igKey => {
+                return [...Array(props.ingredients[igKey])].map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey} />
+                });
+            });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            { tansformedIngedients }
             <BurgerIngredient type="bread-bottom" />
-
         </div>
     );
 };
